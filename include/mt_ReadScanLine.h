d741521233d1ded383d2c34e2604aafb2fe230dc
DG-1337 <devgopal745@gmail.com>
2025-03-21 20:54:54 -0400

    Created two files for multi-thread and single-threaded ReadScanLine implementation

--- include/mt_ReadScanLine.h
+++ include/mt_ReadScanLine.h
@@ -8,28 +8,6 @@ struct Pixel {
     unsigned r, g, b, a;
 };
 
-// creates a blue gradient png to use for testing
-void createTestPNG(const string fileName, unsigned w, unsigned h) {
-    vector <unsigned char> image(w * h * 4); 
-
-    for (unsigned y = 0; y < h; ++y) {
-        for (unsigned x = 0; x < w; ++x) {
-            unsigned index = (y * w + x) * 4;
-
-            image[index] = x * w; 
-            image[index + 1] = y * 16; 
-            image[index + 2] = 128; 
-            image[index + 3] = 255; 
-        }
-    }
-    unsigned err = lodepng::encode(fileName, image, w, h); 
-    if(err) {
-        cout << "Encoding error " << err << ": " << lodepng_error_text(err) << endl;
-    } else {
-        cout << "Image saved as " << fileName << endl;
-    }
-}
-
 // reads a png line by line and returns its rbga pixel values in a 2d vector representing the image 
 vector<vector<Pixel> > readScanLines(const string fileName) {
     vector<unsigned char> image;
@@ -57,25 +35,8 @@ vector<vector<Pixel> > readScanLines(const string fileName) {
         }
         cout << endl;
     }
-
+    
     return pixels;
 }
 
 
-int main(void) {
-    const char* newFileName = "test_image.png";
-
-    createTestPNG(newFileName, 4, 4); 
-    vector<vector<Pixel> > pixels = readScanLines(newFileName); 
-
-    // used to show the vector values after reading each scanline
-    // for (size_t y = 0; y < pixels.size(); y++) {
-    //     for (size_t x = 0; x < pixels[y].size(); x++) {
-    //         Pixel p = pixels[y][x];
-    //         cout << "(" << (int)p.r << "," << (int)p.g << "," << (int)p.b << "," << (int)p.a << ") ";
-    //     }
-    //     cout << endl; 
-    // }
-    
-    return 0; 
-}
