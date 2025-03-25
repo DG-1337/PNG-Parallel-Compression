8d8f2659e2155b8daaebd3acc4c180ca681fb0a7
DG-1337 <devgopal745@gmail.com>
2025-03-25 10:30:39 -0400

    Added prototypes for each function

--- include/filter.h
+++ include/filter.h
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+void applyNoneFilter();
+void applySubFilter(vector<unsigned char>& image, unsigned width, unsigned currentRow);
+void applyUpFilter(vector<unsigned char>& image, unsigned width, unsigned currentRow);
+void applyAverageFilter(vector<unsigned char>& image, unsigned width, unsigned currentRow);
+void applyPaethFilter(vector<unsigned char>& image, unsigned width, unsigned currentRow);
+int paethPredictor(int a, int b, int c);
+
 void applyNoneFilter()
 {
     return;
