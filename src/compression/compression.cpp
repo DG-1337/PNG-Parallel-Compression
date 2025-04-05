66de924e2fd096a2fb35f8f495bc9d1fd200d839
DG-1337 <devgopal745@gmail.com>
2025-04-05 00:54:13 -0400

    Fixed bug in compression that made all alpha values 0

--- src/compression/compression.cpp
+++ src/compression/compression.cpp
@@ -1,29 +1,31 @@
 #include "../../include/compression.h"
-
 vector<unsigned char> compress_image(const vector<unsigned char> &input) {
     cout << "Original size: " << input.size() << endl;
+
+    uLongf compressed_size = compressBound(input.size());
+    vector<unsigned char> compressed(compressed_size); 
 
     z_stream stream{};
     if (deflateInit2(&stream, Z_BEST_COMPRESSION, Z_DEFLATED, -15, 8, Z_DEFAULT_STRATEGY) != Z_OK) {
         throw runtime_error("deflateInit2 failed");
     }
 
+    memset(&stream, 0, sizeof(stream));
+    deflateInit(&stream, Z_BEST_COMPRESSION);
+
     stream.next_in = const_cast<Bytef *>(input.data());
     stream.avail_in = input.size();
+    stream.next_out = compressed.data(); 
+    stream.avail_out = compressed_size; 
 
-    vector<unsigned char> compressed_data(input.size() + (input.size() / 10) + 12);
-    stream.next_out = compressed_data.data();
-    stream.avail_out = compressed_data.size();
+    int result = deflate(&stream, Z_FINISH);
 
-    if (deflate(&stream, Z_FINISH) != Z_STREAM_END) {
+    if(result != Z_STREAM_END) {
         deflateEnd(&stream);
-        throw runtime_error("Compression failed");
+        throw runtime_error("Zlib compression failed while trying to preform deflate()");
     }
 
-    compressed_data.resize(stream.total_out);
     deflateEnd(&stream);
-
-    cout << "Compressed size: " << compressed_data.size() << endl;  
-
-    return compressed_data;
+    compressed.resize(stream.total_out);
+    return compressed;
 }
